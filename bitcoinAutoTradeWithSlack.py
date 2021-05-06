6e873dc381612e3303ce293c80d275cd7bf66295
Yongyong-k <yongwoo177@gmail.com>
2021-05-06 15:26:59 +0900

    BTC_version

--- bitcoinAutoTradeWithSlack.py
+++ bitcoinAutoTradeWithSlack.py
@@ -55,23 +55,23 @@ post_message(myToken,"#slack123", "autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XRP")
+        start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XRP", 0.5)
-            ma15 = get_ma15("KRW-XRP")
-            current_price = get_current_price("KRW-XRP")
+            target_price = get_target_price("KRW-BTC", 0.5)
+            ma15 = get_ma15("KRW-BTC")
+            current_price = get_current_price("KRW-BTC")
             if target_price < current_price and ma15 < current_price:
-                krw = get_balance("KRW")
+                krw = get_balance("BTC")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-XRP", krw*0.9995)
-                    post_message(myToken,"#slack123", "XRP buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    post_message(myToken,"#slack123", "BTC buy : " +str(buy_result))
         else:
-            xrp = get_balance("XRP")
-            if xrp > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-XRP", xrp*0.9995)
-                post_message(myToken,"#slack123", "XRP buy : " +str(sell_result))
+            btc = get_balance("BTC")
+            if BTC > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                post_message(myToken,"#slack123", "btc buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
